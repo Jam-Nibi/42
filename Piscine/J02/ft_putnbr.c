46bde9de75d19ce5d99d5c86bea542bca548e59a
Jamal EL MOUDNIBI <jamal@jamals-macbook-pro.home>
2018-05-08 12:52:28 +0200

    nouveau commit

--- Piscine/J02/ft_putnbr.c
+++ Piscine/J02/ft_putnbr.c
@@ -1,13 +1,21 @@
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+#include "ft_putchar.h"
 
 void	ft_putnbr(int nb)
 {
 	char	a;
+	char	c;
+
+	c = nb;
+	while(c /= 10)
+		a *= 10;
+
+	while(a)
+	{
+		c = nb / a;
+		ft_putchar(c + 48);
+		c = nb - c * a;
+		a /= 10;
+	}
 	
 
 }
--- Piscine/J02/main.c
+++ Piscine/J02/main.c
@@ -1,15 +1,24 @@
-//#include "ft_print_number.c"
+#include "ft_print_number.h"
 #include "ft_print_comb2.h"
-//#include "ft_print_comb.h"
+#include "ft_print_comb.h"
+#include "ft_putnbr.h"
 
-//void	ft_print_comb();
+void	ft_print_comb();
+void	ft_print_comb2();
+void	ft_print_number();
+void	ft_putnbr();
 
-int		main()
+int		main(int argv, char** argc)
 {
+	if(argv == 1)
+	{
+
+	}
 	//ft_print_number();
 	//ft_is_negative();
 	//ft_print_comb();
-	ft_print_comb2();
+	//ft_print_comb2();
+	ft_putnbr(1989);
 
 	return 0;
 }
